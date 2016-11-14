586daa00b31e40c551b8016a754df00e51874add
Lubenchenko Dmytro <d.lubenchenko@gmail.com>
2016-11-14 17:12:21 +0000

    first commit

--- js/index.js
+++ js/index.js
@@ -4,8 +4,6 @@ $(document).ready(function() {
 });
 
 function f_acc(){
-//скрываем все кроме того, что должны открыть
-  $('.projects-1-1 .projects-1-2, .projects-2-1 .projects-2-2, .projects-3-1 .projects-3-2, .projects-4-1 .projects-4-2').not($(this).next()).slideUp(1000);
-// открываем или скрываем блок под заголовоком, по которому кликнули
+  $('.projects-1-1 .projects-1-2, .projects-2-1 .projects-2-2, .projects-3-1 .projects-3-2, .projects-4-1 .projects-4-2').not($(this).next()).slideUp(100);
     $(this).next().slideToggle(2000);
 }
