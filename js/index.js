c009f9872ceda333fb76e89036b806a8b5159d30
Lubenchenko Dmytro <d.lubenchenko@gmail.com>
2016-11-14 18:47:05 +0000

    first commit

--- js/index.js
+++ js/index.js
@@ -6,4 +6,24 @@ $(document).ready(function() {
 function f_acc(){
   $('.projects-1-1 .projects-1-2, .projects-2-1 .projects-2-2, .projects-3-1 .projects-3-2, .projects-4-1 .projects-4-2').not($(this).next()).slideUp(100);
     $(this).next().slideToggle(2000);
-}
+};
+
+// $(function() {
+//     $('#submit').click(function(e) {
+//         e.preventDefault();
+//         $.ajax({
+//             url: "https://formspree.io/d.lubenchenko@gmail.com",
+//             method: "POST",
+//             data: {
+//                 name: $('#name').val()
+//             },
+//             dataType: "json"
+//         })
+//         .done(function() {
+//             $('form').html('Thanck you!');
+//         })
+//         .fail(function() {
+//             $('form').html('Thanck you!');
+//         });
+//     });
+// });
